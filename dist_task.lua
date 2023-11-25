a00c2ceaf7dd686ae2a384cfb72d223c0d08c199
Herr bucketsize <bucket.size@gmail.com>
2023-11-26 00:33:21 +0530

    redesign

--- dist_task.lua
+++ dist_task.lua
@@ -4,19 +4,39 @@ local Ut = require("minilib.util")
 local M = require("minilib.monad")
 local distro_map = {
 	["Debian"] = {
-		pm = "apt install --no-install-recommends %s",
+		pm_install = "apt install --no-install-recommends %s",
 		pm_update = "apt update",
 		pm_search = "apt-cache search %s",
 		pm_installed = "dpkg -L | grep '^ii' |",
 		pm_pkgfile = "apt-file search '%s'",
 	},
 }
+local pkg_alias = {
+	["Debian"] = {
+		alacritty = "alacritty",
+		bspwm = "bspwm",
+		sxhkd = "sxhkd",
+		feh = "feh",
+		lxpolkit = "lxpolkit",
+		tint2 = "tint2",
+		picom = "picom",
+		dunst = "dunst",
+		lemonbar = "lemonbar",
+	},
+}
+local Cache = {
+	instance = Sh.lsb_release(),
+}
+
 local T = {}
 function T.pkgfile(file)
-	local inst = Sh.lsb_release()
+	local inst = Cache.instance
 	local pm = distro_map[inst.distro]
 	if pm then
-		local s, e, ls = Sh.sh(string.format(pm.pm_pkgfile, file))
+		if type(file) == "table" then
+			file = M.List.of(file):join(" ")
+		end
+		local s, e, ls = Sh.sh(string.format(pm.pm_install, file))
 		if not s then
 			print("pkgfile failed", e)
 			return {}
@@ -31,11 +51,37 @@ function T.pkgfile(file)
 		return {}
 	end
 end
+function T.pkgalias(name)
+	local inst = Cache.instance
+	local pm = distro_map[inst.distro]
+	if not pm then
+		print("unsupported distro", inst.distro)
+		return nil
+	end
+	return pkg_alias[inst.distro][name]
+end
+function T.pkginst(name)
+	print("#pkginst", name)
+	local inst = Cache.instance
+	local pm = distro_map[inst.distro]
+	if not pm then
+		print("unsupported distro", inst.distro)
+		return {}
+	end
+	if type(name) == "table" then
+		name = M.List.of(name):join(" ")
+	end
+	local s, e, ls = Sh.sh(string.format(pm.pm_pkgfile, name))
+	if not s then
+		print("pkginst failed", e)
+		return {}
+	end
+	return { pkg = name, status = 0 }
+end
 
 local function test()
 	T.pkgfile("pactl")
+	T.pkginst("tmux")
 end
-
-test()
 
 return T
